d3e655bf53ca478a3a1eaf04a4c4c7f119f3346d
Advitya-sharma <advityasharma21@gmail.com>
2019-12-23 23:24:02 +0530

    ad revenue

---
+++ greedy_algorithms/dot_product.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+long long max_dot_product(vector<int> a, vector<int> b)
+{
+
+  std::sort(a.begin(), a.end(), [](int x, int y) { return x > y; });
+  std::sort(b.begin(), b.end(), [](int x, int y) { return x > y; });
+
+  long long result = 0;
+  for (size_t i = 0; i < a.size(); i++)
+  {
+    result += ((long long)a[i]) * b[i];
+  }
+  return result;
+}
+
+int main()
+{
+  size_t n;
+  std::cin >> n;
+  vector<int> a(n), b(n);
+  for (size_t i = 0; i < n; i++)
+  {
+    std::cin >> a[i];
+  }
+  for (size_t i = 0; i < n; i++)
+  {
+    std::cin >> b[i];
+  }
+  std::cout << max_dot_product(a, b) << std::endl;
+}
