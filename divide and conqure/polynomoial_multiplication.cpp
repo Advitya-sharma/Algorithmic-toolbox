51a88192cbd6d6bbf9454b92c91481a0617b803f
Advitya-sharma <advityasharma21@gmail.com>
2019-12-28 19:31:01 +0530

    fixed poly mul

--- divide and conqure/polynomoial_multiplication.cpp
+++ divide and conqure/polynomoial_multiplication.cpp
@@ -8,16 +8,16 @@ int main()
     int a[3] = {3, 2, 5};
     int b[3] = {5, 1, 2};
 
-    int product[(2 * n) - 1];
+    int product[2 * n - 1];
 
-    for (int i = 0; i <= (2 * n) - 2; i++)
+    for (int i = 0; i <= 2 * n - 2; i++)
     {
         product[i] = 0;
     }
 
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - 1; j++)
+        for (int j = 0; j < n; j++)
         {
             product[i + j] = product[i + j] + (a[i] * b[j]);
         }
